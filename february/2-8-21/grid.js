b016747494d99c1893f4c529468c68b52cb0d4f9
Jared Jewell <jaje9434@colorado.edu>
2021-02-09 11:44:40 -0700

    messed around a lil

--- february/2-8-21/grid.js
+++ february/2-8-21/grid.js
@@ -1,7 +1,7 @@
 const resolution = 20;
 var numColumns, numRows;
 let z = 0;
-let noiseInc = 0.008;
+let noiseInc = 0.005;
 function setup() {
     createCanvas(600, 600);
     rectMode(CENTER);
@@ -19,25 +19,25 @@ function draw() {
     const grid = Array.from({ length: numRows }, () =>
         Array.from({length:numColumns}, () => false)
     )
-    // console.log(grid.length);
-    // console.log(grid[0].length)
-    // grid[1][10] = 10
 
     for (let x = 0; x < grid.length; x++) {
         for (let y = 0; y < grid[0].length; y++) {
-            grid[x][y] = map(noise(x,y,z),0,1,-180,180)
+            // grid[x][y] = map(noise(x,y,z),0,1,-180,180)
+            grid[x][y] += map(noise(x,y,z),0,1,-360,360) //option 1
         }
     }
-    z+=noiseInc
-
+    
     for (let x = 0; x < grid.length; x++) {
         for (let y = 0; y < grid[0].length; y++) {
+            // grid[x][y] = map(noise(x,y,z),0,1,-360,360) //option 2
             push(); 
+            let squareSize = map(noise(x,y,z),0,1,20,25)
             translate((x * width / resolution), y * height / resolution);
             rotate(grid[x][y]);
             fill(map(noise(x,y,z),0,1,0,100), 80, 60, 50)
-            rect(0, 0, 20, 20);
+            rect(0, 0, squareSize, squareSize);
             pop();            
         }
     }
+    z+=noiseInc
 }
